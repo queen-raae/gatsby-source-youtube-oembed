751cba563aac7286173645149bc3ef899e9782dc
Benedicte Raae <raae@raae.codes>
2022-02-17 18:18:47 +0100

    fix: improved sourcing of remote image files (#3)
    * fix: remove use of depricated __NODE
    
    * chore: add logging
    
    * fix: move file creation to onCreateNode

--- plugin/gatsby-node.js
+++ plugin/gatsby-node.js
@@ -22,35 +22,60 @@ const fetchEmbed = async (id) => {
 const prepYouTubeNode = async (gatsbyUtils, id) => {
   const {
     actions: { createNode },
-    getCache,
     createNodeId,
     createContentDigest,
+    reporter,
   } = gatsbyUtils;
 
   const youTubeNodeId = createNodeId(`you-tube-${id}`);
   const embedData = await fetchEmbed(id);
 
-  const imageFile = await createRemoteFileNode({
-    // The source url of the remote file
-    url: embedData.thumbnail_url,
-    parentNodeId: youTubeNodeId,
-    getCache,
-    createNode,
-    createNodeId,
-  });
-
   createNode({
     id: youTubeNodeId,
+    youTubeId: id,
     oEmbed: embedData,
-    thumnail___NODE: imageFile.id,
     internal: {
       type: `YouTube`,
       contentDigest: createContentDigest(embedData),
     },
   });
+
+  reporter.info(`Created YouTube Node for ${id}`);
 };
 
 exports.sourceNodes = async (gatsbyUtils, pluginOptions) => {
   const { youTubeIds = [] } = pluginOptions;
   await Promise.all(youTubeIds.map((id) => prepYouTubeNode(gatsbyUtils, id)));
+};
+
+exports.onCreateNode = async (gatsbyUtils) => {
+  const { node, actions, reporter, createNodeId, getCache } = gatsbyUtils;
+  const { createNodeField, createNode } = actions;
+
+  if (node.internal.type === `YouTube`) {
+    const imageFile = await createRemoteFileNode({
+      // The url of the remote file
+      url: node.oEmbed.thumbnail_url,
+      parentNodeId: node.id,
+      getCache,
+      createNode,
+      createNodeId,
+    });
+
+    createNodeField({
+      node,
+      name: `thumbnailFileId`,
+      value: imageFile.id,
+    });
+
+    reporter.info(`Created YouTube File Node for ${node.youTubeId} thumbnail`);
+  }
+};
+
+exports.createSchemaCustomization = ({ actions }) => {
+  actions.createTypes(`
+    type YouTube implements Node {
+      thumbnail: File @link(from: "fields.thumbnailFileId")
+    }
+  `);
 };
