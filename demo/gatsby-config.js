9c0279abe09eb70260edb3beec927cda3459107d
Benedicte Raae <raae@raae.codes>
2022-03-24 18:44:50 +0100

    chore(demo): add videos

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -3,7 +3,12 @@ module.exports = {
     {
       resolve: "@raae/gatsby-source-youtube-oembed",
       options: {
-        youTubeIds: ["Bk1jonYPFD4", "TzJfepDjpzM"],
+        youTubeIds: [
+          "Bk1jonYPFD4",
+          "TzJfepDjpzM",
+          "UsSJ_QNp6uo",
+          "TX5XPuHhz9o",
+        ],
       },
     },
     "gatsby-plugin-sharp",
